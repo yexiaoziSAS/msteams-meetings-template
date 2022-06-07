0f1c144a40fe88607c111d14b4e006e84198b13a
yexiaoziSAS <79674263+yexiaoziSAS@users.noreply.github.com>
2022-06-07 20:39:51 +0800

    Update msalApp.ts

--- src/auth/msalApp.ts
+++ src/auth/msalApp.ts
@@ -13,7 +13,7 @@ function isIE() {
 
 export const msalApp = new UserAgentApplication({
   auth: {
-    clientId: '47ee545d-7bd2-47de-b86e-586333b4145c', // TODO: move this into a cfg value from composition root
+    clientId: '069c5559-40b6-4ff5-9fa2-852c91248973', // TODO: move this into a cfg value from composition root
     authority: 'https://login.microsoftonline.com/common',
     validateAuthority: true,
     postLogoutRedirectUri: 'http://localhost:3000',
